efa302f78d18a9671daa2660ec2cb758a395fa55
Olabiran Joshua Olaiya <olabiranj@gmail.com>
2022-01-13 14:45:46 +0100

    commit

--- src/pages/Tweets.tsx
+++ src/pages/Tweets.tsx
@@ -3,7 +3,6 @@ import { TwitterTimelineEmbed } from "react-twitter-embed";
 const Tweets = () => {
   return (
     <section className="">
-      <p>Tweets are not visible to Nigerians</p>
       <div className="col-sm-8 mx-auto mt-3">
         <TwitterTimelineEmbed
           sourceType="profile"
